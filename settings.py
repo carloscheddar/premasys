0299cc322dfe5eac7d44cbc7d4ba4b37869c7fa9
cfeliciano <c.feliciano2009@gmail.com>
2013-10-18 17:02:29 -0400

    Added the Procfile for Heroku deployment and added requirements

--- settings.py
+++ settings.py
@@ -176,6 +176,16 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
 )
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 #Add support for local settings
 try:
    from settings_local import *
