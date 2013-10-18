b436fbcd10051e73f8344bb8aa8c09eac03ec5e5
cfeliciano <c.feliciano2009@gmail.com>
2013-10-18 18:41:57 -0400

    Used settings_local to set up the DB and removed debug from settings

--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 import os, sys
 # Django settings for premasys project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
     )
@@ -20,8 +17,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, '%s.sqlite' % PROJECT_NAME), # Or path to database file if using sqlite3.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
