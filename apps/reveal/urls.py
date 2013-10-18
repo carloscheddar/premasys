60725207bcaed7cf4fd6274ed50ca3556c5c0b3c
cfeliciano <c.feliciano2009@gmail.com>
2013-10-18 14:14:03 -0400

    Deleted reveal app

--- apps/reveal/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from reveal.views import Reveal
-
-urlpatterns = patterns("",
-    url(r"^$", Reveal, name="presentation_view")
-)
--- apps/reveal/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-def Reveal(request):
-	return render_to_response("index.html", RequestContext(request, {'title': 'Test Reveal'} ) )
