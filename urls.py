9879b3d085e9f94570c92aece30f8ccf9f9d7a6b
cfeliciano <c.feliciano2009@gmail.com>
2013-10-18 23:55:09 -0400

    Added static folder fix for heroku

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,8 @@ urlpatterns = patterns('',
     url(r"^reveal/$", include("reveal.urls")),
     url(r'^accounts/', include('registration.backends.default.urls')),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
