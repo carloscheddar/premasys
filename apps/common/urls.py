9dc26d3bfaeae961f1a771ced7a768c892847218
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-10-23 10:47:08 -0400

    Changed the base template to use bootstrap 3

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from common.views import Common
+from common.views import Common, Login
 
 urlpatterns = patterns("",
-    url(r"^$", Common, name="base")
+    url(r"^$", Common, name="base"),
+    url(r"^login/", Login, name="login")
 )
--- apps/common/views.py
+++ apps/common/views.py
@@ -4,3 +4,6 @@ from django.template import RequestContext
 
 def Common(request):
   return render_to_response("base.html", RequestContext(request, {'title': 'Premasys Test'} ) )
+
+def Login(request):
+  return render_to_response("login.html", RequestContext(request, {'title': 'Premasys Test'} ) )
