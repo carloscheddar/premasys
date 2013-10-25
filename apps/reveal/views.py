57efd70d920aaa63beb9eff30307c99440e83cd8
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-10-25 13:33:11 -0400

    Added the preliminary edit and model files to reveal

--- apps/reveal/views.py
+++ apps/reveal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from reveal.models import Lesson
 
 def Reveal(request):
 	return render_to_response("index.html", RequestContext(request, {'title': 'Test Reveal'} ) )
