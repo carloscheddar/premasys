a4f4c8331ee8ccd7933ed762d61e47d461297b31
karlo.luis.m@gmail.com <enter@Djoodle.(none)>
2013-10-27 20:39:18 -0400

    Fix on views missing file

---
+++ apps/common/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def Common(request):
+    return render_to_response("base.html", RequestContext(request, {'title': 'Premasys Test'}))
