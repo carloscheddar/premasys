50c6acbd1f754735e31c2d39728f8a36565e1ddc
cfeliciano <c.feliciano2009@gmail.com>
2013-10-28 01:23:30 -0400

    Added raw lesson output to the user page and checked if the user has a lesson else output an empty string.

--- apps/reveal/models.py
+++ apps/reveal/models.py
@@ -4,3 +4,6 @@ from registration.models import User
 class Lesson(models.Model):
 	username = models.ForeignKey(User, null=True, blank=True)
 	slide = models.TextField(blank=True, null=True)
+
+	def __unicode__(self):
+		return u'%s'  % self.username
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from registration.models import User
+from reveal.models import Lesson
 
 def Profile(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
+	try:
+		lesson = Lesson.objects.get(username=user).slide
+	except:
+		lesson = ''
 	return render_to_response("users/profile.html", RequestContext(request, {
 		'title': 'Profile',
-		'user_page' : user
+		'user_page' : user,
+		'lesson' : lesson
 		}))
