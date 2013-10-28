dbeeed95954ac3e5b9d49bc5003a651839cdac38
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-10-28 18:04:39 -0400

    Saved slides can now be showed. Also added preliminary edit views.

--- apps/reveal/urls.py
+++ apps/reveal/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
-from reveal.views import Reveal, Edit, Save
+from reveal.views import Reveal, Edit, Save, Create, Show
 
 urlpatterns = patterns("",
     url(r"^$", Reveal, name="presentation_view"),
-    url(r"edit", Edit, name="Edit View"),
+    url(r"create", Create, name="Create View"),
+    url(r"edit/(?P<lesson_id>\d+)$", Edit, name="Edit View"),
     url(r"save", Save, name="Save View"),
+    url(r"show/(?P<lesson_id>\d+)$", Show, name="Show View"),
 )
--- apps/reveal/views.py
+++ apps/reveal/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from reveal.models import Lesson
 
 def Reveal(request):
 	return render_to_response("index.html", RequestContext(request, {'title': 'Test Reveal'} ) )
 
-def Edit(request):
-  return render_to_response("edit.html", RequestContext(request, {'title': 'Edit'}))
+def Create(request):
+  return render_to_response("create.html", RequestContext(request, {'title': 'Edit'}))
 
 def Save(request):
 	post = request.POST.keys()[0]
 	Lesson(username=request.user, slide=post).save()
 	return render_to_response("save.html", RequestContext(request,{'h': 'hello'}))
+
+def Show(request, lesson_id):
+	slide = get_object_or_404(Lesson, pk=lesson_id)
+	return render_to_response("show.html", RequestContext(request, {'slide': slide}))
+
+def Edit(request, lesson_id):
+	slide = get_object_or_404(Lesson, pk=lesson_id)
+	return render_to_response("edit.html", RequestContext(request, {'slide': slide}))
