cb351c89bf4dd0a168e6e98a07baa405ba1922f7
karlo.luis.m@gmail.com <enter@Djoodle.(none)>
2013-10-27 20:00:47 -0400

    further models development.

--- apps/common/models.py
+++ apps/common/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here
 
-
-
+class User(models.Model):
+	UserID = models.BigIntegerField(primary_key = True)
+	Username = models.CharField(max_length = 30)
+	email = models.EmailField(max_length=75)
+	perm = models.BooleanField(initial = false)
+	
 class Course(models.Model):
 	CourseName = models.CharField(max_length = 260, blank = False)
 	CourseID = models.BigIntegerField(primary_key = True)
-
+	
+class User_Course(models.Model):
+	Prof_CourID = models.BigIntegerField(primary_key = True)
+	UserID = models.ForeignKey('User')
+	CourseID = models.ForeignKey('Course')
 
 class Lessons(models.Model):
 	LessonID = models.BigIntegerField(primary_key = True)
