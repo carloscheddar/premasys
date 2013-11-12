8f1b2088538138ca6da634fd674ca7afbbb00220
karlo.luis.m@gmail.com <enter@Djoodle.(none)>
2013-11-12 03:20:55 -0400

    Extended the models to add the use of quizes

--- apps/common/models.py
+++ apps/common/models.py
@@ -2,24 +2,31 @@ from django.db import models
 
 # Create your models here
 
+#Table containing the users data.
 class User(models.Model):
 	UserID = models.BigIntegerField(primary_key = True)
-	Username = models.CharField(max_length = 30)
+	Username = models.CharField(unique = True, max_length = 30)
 	
 	Email = models.EmailField(max_length=75)
 	
+#Table containing the existing courses and it's data
 class Course(models.Model):
 	CourseName = models.CharField(max_length = 260, blank = False)
 	CourseID = models.BigIntegerField(primary_key = True)
 	
 	Description = models.TextField()
-	
+
+#Table containing relations between Users and Courses
+#If a relation exists then the user can view the specified course
+#This can also specify if the user can edit content
 class User_Course(models.Model):
 	UserID = models.ForeignKey('User')
 	CourseID = models.ForeignKey('Course')
 	
 	PermEdits = models.BooleanField(default = False)
 
+#Table containing existing lessons and it's data
+#It also holds a relation to a course, this value cannot be null
 class Lessons(models.Model):
 	LessonID = models.BigIntegerField(primary_key = True)
 	CourseID = models.ForeignKey('Course')
@@ -27,8 +34,50 @@ class Lessons(models.Model):
 	
 	Title = models.TextField()
 
+#Table containing existing slides
+#It also holds a relation to the lesson it is part of
 class Slides(models.Model):
 	SlideID = models.BigIntegerField(primary_key = True)
 	Number = models.BigIntegerField()
-	Data = models.TextField()
+
 	LessonID = models.ForeignKey('Lessons')
+
+#Table containing the content for a slide
+#Multiple content input can be created for a single slide
+#
+class Content(models.Model):
+	ContentID = models.BigIntegetField(primary_key = True)
+	Type = models.CharField(max_length = 10)
+       	Data = models.TextField()
+
+	SlideID = models.ForeignKey('Slides')
+
+
+#Table containing the different tags
+#Need to specify if tags are selected by course, lecture or slide
+#Temporarily has been set to lesson
+class Tag_Content:
+	TagID = models.BigIntegerField(primary_key = True)
+	Type = models.CharField(max_length = 10)
+
+	LessonID = models.ForeignKey('Lesson')
+
+#
+class Quiz(models.Model):
+	QuizID = models.BigIntegerField(primary_key = True)
+	
+	#either create a table with questions or set an attribute containing it.
+	
+	TagID = models.ForeignKey('Tag_Content')
+	ContentID = models.ForeignKey('Content')
+
+#For discussion, is there a need for questions or will multiple 'quiz' do?
+#class Questions(models.Model)
+
+#
+class Answers(models.Model):
+	AnswersID = models.BigIntegerField(primary_key = True)
+	Correct = models.BooleanField(default = False)
+	Text = models.TextField()
+	
+	QuizID = model.ForeignKey('Quiz')
--- apps/reveal/views.py
+++ apps/reveal/views.py
@@ -7,6 +7,7 @@ from common.models import Course
 from common.models import User_Course
 from common.models import Lessons
 from common.models import Slides
+from common.models import Content
 
 
 def Reveal(request):
@@ -28,22 +29,47 @@ def Save(request):
 # returns data pulled
 ###----------------------------###
 
+#SUMBIT LESSON TO DB
+
 def Save_Lesson(lesson):
 	ID = lesson["LessonID"]
 	#Lesson(LessionId = ID).save()
 	for slide in lesson["Slides"]:
-		Slides(LessonID = ID, Number = slide["Number"], Data = slide["Data"]).save()
-
+		Slides(LessonID = ID, Number = slide["Number"]).save()
+		
+		for content in slide["Content"]:
+			Content(SlideID=slide["SlideID"], Type = content["Type"], Data = content["Data"]).save()
 
 	return lesson
-#
+
+
+#FIND LESSON AND SLIDES FUNCTIONS
 
 def Get_Slides(L_ID):
 	return sorted(Slides.objects.all().filter(LessonID = L_ID), key =lambda item: item['Number'])
+#Revisar para usar Content() model para conseguir la info
+#slide["Content"] = Content.Objects.all().filter(SlideID = slide["SlideID"]
+
 
 def Get_Lesson(L_ID):
 	lesson = Lesson.objects.filter(LessonID = L_ID)
 	lesson["Slides"] = Get_Slides(L_ID)
 
 	return lesson
+
 	
+### VIEW A COURSE OR LESSON FUNCTIONS
+
+def Permit_View(C_ID, U_ID):
+	Check_Course = User_Course.objects.filter(CourseID = C_ID, UserID = U_ID)
+	if():#check if relation user to course exists
+		return (True, Check_Course["PermEdits"]) #If it does exist return the True and the permit value
+	else:#there is no relation
+		return (False, False)
+
+def Permit_Edit(C_ID, U_ID):
+	Check_Course = User_Course.objects.filter(CourseID = C_ID, UserID = U_ID)
+	if(): #check if relation user to course exists
+		return Check_Course["PermEdits"]) #If it does exist return the permit value
+	else: #there is no relation
+		return False
