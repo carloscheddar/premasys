6e83cd710217d13f009f3c0674c4b89637327196
cfeliciano <c.feliciano2009@gmail.com>
2013-12-16 04:22:31 -0400

    Updated edit templates to use angular just like create.

--- apps/reveal/static/slides/edit.js
+++ apps/reveal/static/slides/edit.js
@@ -1,4 +1,32 @@
+var template = '<section>\n' + "<h1>Write</h1>\n<h2>Here!</h2>\n" + '</section>';
 angular.module('reveal', ['ngSanitize'])
-  .controller('Ctrl', ['$scope', function Ctrl($scope) {
-    $scope.slideshow = slides;
-  }]);
+  .controller('Ctrl', ['$scope',
+    function Ctrl($scope) {
+      $scope.counter = 0;
+      $scope.slideshow = textarea[0];
+      $scope.slide = function(count) {
+        $scope.counter = count;
+        if (textarea[count]) {
+          $scope.slideshow = textarea[count];
+        } else{
+          $scope.slideshow = template;
+        }
+        console.log(textarea);
+      };
+      $scope.update = function() {
+        textarea[$scope.counter] = $scope.slideshow;
+        Reveal.toggleOverview();
+        Reveal.toggleOverview();
+      };
+      $scope.save = function() {
+        var json = JSON.stringify(textarea);
+        $.ajax({
+            type: "POST",
+            url: '/reveal/save',
+            data: json,
+            headers: { "X-CSRFToken": getCookie("csrftoken") }
+        });
+        console.log("Saved");
+      };
+    }
+  ]);
