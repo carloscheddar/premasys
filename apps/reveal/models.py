cb8e2e7ac5949ac64f6c0d18b5b7a731ab1a2854
cfeliciano <c.feliciano2009@gmail.com>
2013-12-16 03:31:55 -0400

    Some pep8 fixes.

--- apps/reveal/models.py
+++ apps/reveal/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from registration.models import User
 
-class Lesson(models.Model):
-	username = models.ForeignKey(User, null=True, blank=True)
-	slide = models.TextField(blank=True, null=True)
 
-	def __unicode__(self):
-		return u'%s'  % self.username
+class Lesson(models.Model):
+    username = models.ForeignKey(User, null=True, blank=True)
+    slide = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s' % self.username
