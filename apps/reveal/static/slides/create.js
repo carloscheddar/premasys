cc40a5b819db79764e3d7029d32dc1498d141dff
cfeliciano <c.feliciano2009@gmail.com>
2013-12-16 01:04:07 -0400

    textbox can now iterate through slides.

--- apps/reveal/static/slides/create.js
+++ apps/reveal/static/slides/create.js
@@ -1,1 +1,16 @@
-angular.module('reveal', ['ngSanitize']);
+var textarea = [];
+textarea[0] = "<h1>Hello</h1>";
+textarea[1] = "<h2>Welcome</h2>";
+
+angular.module('reveal', ['ngSanitize'])
+  .controller('Ctrl', ['$scope',
+    function Ctrl($scope) {
+      $scope.counter = 0;
+      $scope.slideshow = '<section>' + textarea[$scope.counter] + '</section>';
+      $scope.slide = function(count) {
+        $scope.counter = count;
+        $scope.slideshow = '<section>' + textarea[count] + '</section>';
+      };
+    }
+  ]);
+console.log(counter);
