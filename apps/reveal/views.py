3b69ed4518ca8f0175bca5100389d1dca85bd80d
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-12-17 19:05:31 -0400

    Create function to convert json to html

--- apps/reveal/views.py
+++ apps/reveal/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from reveal.models import Lesson
 import ast
-
+import re
 
 def Reveal(request):
     return render_to_response("index.html", RequestContext(request, {'title': 'Test Reveal'}))
@@ -27,6 +27,8 @@ def Save(request):
 
 def Show(request, lesson_id):
     slide = get_object_or_404(Lesson, pk=lesson_id)
+    slide = ast.literal_eval(slide.slide)
+    slide = json2html(slide)
     return render_to_response("show.html", RequestContext(request, {'slide': slide}))
 
 
@@ -34,3 +36,19 @@ def Edit(request, lesson_id):
     slide = get_object_or_404(Lesson, pk=lesson_id)
     slide = ast.literal_eval(slide.slide)
     return render_to_response("edit.html", RequestContext(request, {'slide': slide}))
+
+# Function to convert json to html to be displayed in show
+def json2html(json):
+    string = ''
+    regex = re.compile("\(([^)]+)\)")
+    for j in json:
+        t = j["type"]
+        text = j["text"]
+        if t == "text":
+            arr = regex.findall(text)
+            string += '<section>'
+            for a in arr:
+                split = a.split(' ',1)
+                string += "<" + split[0] + ">" + split[1] + "</" + split[0] + ">"
+            string += '</section>'
+    return string
